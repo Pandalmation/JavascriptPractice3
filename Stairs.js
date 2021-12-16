7f6fda714db56106daeeca72f484d64c9da82eca
Tiffany <91600281+Pandalmation@users.noreply.github.com>
2021-12-16 11:09:07 +0700

    following the logic

--- Stairs.js
+++ Stairs.js
@@ -1,9 +1,17 @@
-function climbingStairs(stepcost) {
-    for (i = stepcost.length - 3; i >= -1; i--)
-        stepcost[i] += Math.min(stepcost[i+1], stepcost[i+2])
-    return Math.min(stepcost[0], stepcost[1])
-};
-climbingStairs([0, 2, 2, 1]) 
-climbingStairs([0, 2, 3, 2]) 
-climbingStairs([10, 15, 20]) 
-climbingStairs([0, 0, 0, 0, 0, 0])
+function climbstairs(stepcost,minstepcost){  
+    let steps = [];
+
+    for (i = 2; i < stepcost.length; i++){
+        steps[0] = stepcost[0];
+    steps[1] = stepcost[1];
+        steps[i] = stepcost[i] + Math.min(steps[i-2], steps[i-1]);
+    }
+    
+    minstepcost = Math.min((steps[stepcost.length-2]),(steps[stepcost.length-1]));                                                                                      
+    console.log(minstepcost);
+}
+
+(climbstairs([0, 2, 2, 1]));
+(climbstairs([0, 2, 3, 2])); 
+(climbstairs([10, 15, 20]));
+(climbstairs([0, 0, 0, 0, 0, 0]));
